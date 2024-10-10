595186598c86a381fa008b471205c613f0322751
Juan De Guzman <juanfrizdeguzman@icloud.com>
2024-10-10 16:29:32 +0100

    restructuring of dropping outliers

--- machineLearning.py
+++ machineLearning.py
@@ -5,13 +5,11 @@ import numpy as np
 
 #Original data
 strava_data = pd.read_csv('raw-data-kaggle.csv')
-
 head = strava_data.head()
-
 nunique_athletes = strava_data.athlete.nunique()
-
 strava_data.dtypes
-
+sns.pairplot(strava_data, vars=['distance (m)', 'elevation gain (m)', 'elapsed time (s)', 'average heart rate (bpm)'])
+plt.savefig('strava_pairplot_before.png', dpi=300)
 print(strava_data.describe())
 
 
@@ -19,20 +17,21 @@ print(strava_data.describe())
 strava_data['pace (min/km)'] = (strava_data['elapsed time (s)'] / 60) /(strava_data['distance (m)'] * 0.001)
 
 
-#Removing Outliers
-strava_data = strava_data.drop(strava_data[strava_data['elapsed time (s)'] >= 180000].index)
-strava_data = strava_data.drop(strava_data[strava_data['elapsed time (s)'] == 0].index)
-strava_data = strava_data.drop(strava_data[strava_data['distance (m)'] == 0].index)
-strava_data = strava_data.drop(strava_data[strava_data['pace (min/km)'] < 2.56333].index) #Fastest pace in the world for 5 km run.
-strava_data = strava_data.drop(strava_data[strava_data['pace (min/km)'] > 15].index) #Based on slow walking speed.
-strava_data = strava_data.drop(strava_data[strava_data['average heart rate (bpm)'] == 'NaN'].index)
-strava_data = strava_data.drop(strava_data[strava_data['average heart rate (bpm)'] == 0].index)
+# Dropping rows based on conditions
+strava_data = strava_data[
+    (strava_data['elapsed time (s)'] < 180000) & 
+    (strava_data['elapsed time (s)'] > 0) & 
+    (strava_data['distance (m)'] > 0) & 
+    (strava_data['pace (min/km)'] >= 2.56333) & 
+    (strava_data['pace (min/km)'] <= 15) & 
+    (strava_data['average heart rate (bpm)'].notnull()) & 
+    (strava_data['average heart rate (bpm)'] > 0)
+]
 
 
 #Analysis of average pace (min/km)
 pace_info = strava_data['pace (min/km)'].describe()
 pace_largest_smallest = strava_data.sort_values(by=['pace (min/km)'], ascending=False)
-
 plt.hist(strava_data['pace (min/km)'], bins=100)
 plt.xlabel('Pace (min/km)')
 plt.ylabel('Frequency')
@@ -58,19 +57,4 @@ print('Current length of data: ' + str(len(strava_data)))
 
 
 sns.pairplot(strava_data, vars=['distance (m)', 'elevation gain (m)', 'elapsed time (s)', 'average heart rate (bpm)'])
-plt.savefig('strava_pairplot.png', dpi=300)
-
-
-
-'''Analyse data, and clean for better performance of ML:
-0. EDA???
-1. Analyse data with pairplots
-    - Categorize the distance (2k, 5k, 10k, 42k, 100k, 100k+)
-2. Check weather to standardize or normalise data, most likely standardize
-    - Perform categorical encoding for M/F
-3. Split data into features and labels
-4. Split data into train and test'''
-
-'''Male or female prediction - using classification analysis'''
-'''5k, 2k, and Marathon time Prediction - multi-modal linear regression'''
-'''Pace Prediciton'''
+plt.savefig('strava_pairplot_after.png', dpi=300)
