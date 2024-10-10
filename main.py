a953e7c7e09f5dad7e015e5791036651b0aec837
Juan De Guzman <juanfrizdeguzman@icloud.com>
2024-10-10 12:55:53 +0100

    Continuation of analysis

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 strava_data = pd.read_csv('raw-data-kaggle.csv')
 
-print(strava_data.head())
+head = strava_data.head()
 
-print(strava_data.athlete.unique())
+unique_athletes = strava_data.athlete.unique()
 
-plt.hist(strava_data['elapsed time (s)'], bins=100)
-plt.show()
+#Adding average pace (min/km)
+
+#Analysis of elapsed time
+elapsed_time_data = strava_data['elapsed time (s)']
+elapsed_info = elapsed_time_data.describe()
+print(elapsed_info)
+
+elapsed_time_largest_smallest = strava_data.sort_values(by=['elapsed time (s)'], ascending=False)
+print(elapsed_time_largest_smallest)
+
+#Analysis of distance
+distance_data = strava_data['distance (m)']
+distance_info = distance_data.describe()
+print(distance_info)
+
+distance_largest_smallest = strava_data.sort_values(by=['distance (m)'], ascending=False)
+print(distance_largest_smallest)
+
+strava_data = strava_data.drop(strava_data[strava_data['elapsed time (s)'] >= 180000].index)
 
 sns.pairplot(strava_data, vars=['distance (m)', 'elevation gain (m)', 'elapsed time (s)', 'average heart rate (bpm)'])
-plt.savefig('Pair_Plot')
+plt.savefig('strava_pairplot.png', dpi=300)
+plt.show() 
+
 
 
 '''Analyse data, and clean for better performance of ML:
