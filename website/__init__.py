d2d0dd9552452c10b568eb39174ffd6d3cf2508b
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-10-10 22:17:08 +0100

    added comments, plus refactored codebase fully into an OOP format

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
-    # import the different routes
-    from .auth import auth
-    from .views import views
+    # # import the different routes
+    # from .auth import auth
+    # from .views import views
 
-    # register blueprints
-    app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(views, url_prefix='/')
+    # # register blueprints
+    # app.register_blueprint(auth, url_prefix='/')
+    # app.register_blueprint(views, url_prefix='/')
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,57 +1,63 @@
+# import necessary modules
 from flask import Blueprint, render_template, request
+from website.baseView import baseView
 from stravalib import Client
 
-auth = Blueprint('auth', __name__)
-
-# setup necessary variables
-client_id, client_secret = open("client_secrets.txt").read().strip().split(",")
-request_scope = ["read_all", "profile:read_all", "activity:read_all"]
-redirect_url = "http://127.0.0.1:5000/strava-oauth" # this is redirect url
-
-# setup routes
-@auth.route("/")
-def login():
-    client = Client()
-    # creates auth url 
-    url = client.authorization_url(
-    client_id=client_id,
-    redirect_uri=redirect_url,
-    scope=request_scope,
-    approval_prompt="auto"
-    )
-    return render_template('login.html', authorize_url = url)
-
-@auth.route("/strava-oauth")
-def logged_in():
-    code = request.args.get("code")
-    client = Client()
-    # gets access token
-    access_token = client.exchange_code_for_token(
-        client_id=client_id,
-        client_secret=client_secret,
-        code=code
-    )
-
-    # activity = client.get_activity(0)
-
-    # gets the athlete
-    activities = client.get_activities()
-    activity_ids = [] # get the unique ids of each activity so we can get the 'detailed' activities object via the 'get_activity()' function
-    for activity in activities:
-        activity_ids.append(activity.id)
-        # print(f"\nActivity ID: {activity.id}")
-        # print(f"Distance (m): {activity.distance}")
-        # print(f"Max Speed (m/s): {activity.max_speed}")
-        # print(f"Elapsed Time (s): {activity.elapsed_time}")
-
-    for x in range(len(activity_ids)):
-        avg_hr = client.get_activity(activity_ids[x]).average_heartrate
-        print(f"Avg Heart Rate: {client.get_activity(activity_ids[x]).average_heartrate}")
+# wrap auth routes in a class for OOP
+class authRoutes(baseView):
+    def __init__(self, flaskApp):
+        super().__init__(flaskApp)
+        # setup necessary variables
+        self.__client_id, self.__client_secret = open("client_secrets.txt").read().strip().split(",")
+        self.__request_scope = ["read_all", "profile:read_all", "activity:read_all"]
+        self.__redirect_url = "http://127.0.0.1:5000/authentication" # this is redirect url
     
-    strava_athlete = client.get_athlete()
+    def _setupRoutes(self):
+        # setup routes
+        @self._flaskApp.route("/")
+        def login(): # shows user a prompt to connect to strava
+            client = Client()
 
-    return render_template(
-        "login_results.html",
-        athlete=strava_athlete,
-        access_token=access_token,
-    )
+            # creates auth url 
+            url = client.authorization_url(
+            client_id=self.__client_id,
+            redirect_uri=self.__redirect_url,
+            scope=self.__request_scope,
+            approval_prompt="auto"
+            )
+
+            return render_template('login.html', authorize_url = url)
+
+        @self._flaskApp.route("/authentication")
+        def logged_in(): # just shows their auth token, will show their data or something in the future
+            code = request.args.get("code")
+            client = Client()
+            
+            # gets access token
+            access_token = client.exchange_code_for_token(
+                client_id=self.__client_id,
+                client_secret=self.__client_secret,
+                code=code
+            )
+
+            # gets the athlete
+            activities = client.get_activities()
+            activity_ids = [] # get the unique ids of each activity so we can get the 'detailed' activities object via the 'get_activity()' function
+            for activity in activities:
+                activity_ids.append(activity.id)
+                # print(f"\nActivity ID: {activity.id}")
+                # print(f"Distance (m): {activity.distance}")
+                # print(f"Max Speed (m/s): {activity.max_speed}")
+                # print(f"Elapsed Time (s): {activity.elapsed_time}")
+
+            for x in range(len(activity_ids)):
+                avg_hr = client.get_activity(activity_ids[x]).average_heartrate
+                print(f"Avg Heart Rate: {avg_hr}")
+            
+            strava_athlete = client.get_athlete()
+
+            return render_template(
+                "login_results.html",
+                athlete=strava_athlete,
+                access_token=access_token,
+            )
