624b565d58dad2508d9c014d5fded8eb94ee101d
Juan De Guzman <juanfrizdeguzman@icloud.com>
2024-11-01 11:10:21 +0000

    Setting up data visualisation page

--- website/auth.py
+++ website/auth.py
@@ -99,3 +99,45 @@ class authRoutes(baseView):
                 access_token=access_token,
                 activity_data=activity_data,
             )
+
+        @self._flaskApp.route("/datavisualisation")
+        def data_visualisation():
+            code = request.args.get("code")
+            client = Client()
+            
+            # gets access token
+            access_token = client.exchange_code_for_token(
+                client_id=self.__client_id,
+                client_secret=self.__client_secret,
+                code=code
+            )
+            activities = client.get_activities()
+            activity_ids = [] # get the unique ids of each activity so we can get the 'detailed' activities object via the 'get_activity()' function
+            activity_data = []
+            for activity in activities:
+                activity_ids.append(activity.id)
+                # print(f"\nActivity ID: {activity.id}")
+                # print(f"Distance (m): {activity.distance}")
+                # print(f"Max Speed (m/s): {activity.max_speed}")
+                # print(f"Elapsed Time (s): {activity.elapsed_time}")
+
+            for x in range(3):
+                averageHeartRate = client.get_activity(activity_ids[x]).average_heartrate
+                distance = client.get_activity(activity_ids[x]).distance
+                elapsedTime = client.get_activity(activity_ids[x]).elapsed_time
+                elevationHigh = client.get_activity(activity_ids[x]).elev_high
+                elevationLow = client.get_activity(activity_ids[x]).elev_low
+                elevationGain = elevationHigh - elevationLow
+                print(averageHeartRate, distance, elapsedTime, elevationGain)
+
+                activity_data.append({
+                    "average_heart_rate": averageHeartRate,
+                    "distance": distance,
+                    "elapsed_time": elapsedTime,
+                    "elevation_gain": elevationGain,
+        })
+
+            return render_template(
+                "data_visuals.html",
+                activity_data=activity_data,
+            )
