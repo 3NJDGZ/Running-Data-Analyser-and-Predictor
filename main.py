5be0b1fcb5be714cdee8232b6ffd1b515ee75411
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-11-09 18:42:47 +0000

    setup basic connection to mongoDB

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # import necessary classes and functions
-from website import create_app
+from website import create_app, createMongoDB
 from website.views import viewRoutes
 from website.auth import authRoutes
 from website.dataVisuals import dataVisualsRoutes
 
+
 # create flask wrapper
-class flaskAppWrapper():
+class flaskAppWrapper:
     def __init__(self):
-        self.__app = create_app() # creates flask application itself
-        self.setupRoutes() # sets up the routes for the browser
-    
+        self.__app = create_app()  # creates flask application itself
+        self.__mongoDB = createMongoDB(self.__app)
+        self.setupRoutes()  # sets up the routes for the browser
+
     def setupRoutes(self):
         # create the different routes, and call their protected methods which are overridden from baseView abstract super class
         views = viewRoutes(self.__app)
@@ -18,11 +20,13 @@ class flaskAppWrapper():
         views._setupRoutes()
         auth._setupRoutes()
         dataVisuals._setupRoutes()
-    
+
     def run(self):
         self.__app.run(debug=True)
+
 
 flaskAppWrapper = flaskAppWrapper()
 
 if __name__ == "__main__":
     flaskAppWrapper.run()
+
--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
+
+with open("mongoURI.txt", "r") as file:
+    mongoURI = file.readline().strip()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config["MONGO_URI"] = mongoURI
     return app
+
+
+def createMongoDB(app):
+    mongoClient = MongoClient(mongoURI)
+    mongo = PyMongo(app)
+    return mongo
