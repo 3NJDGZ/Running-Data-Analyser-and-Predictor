2003b344f63b1b6c591028470c941f0cfaed1054
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-11-12 16:49:28 +0000

    added basic json data insertion and retrieval

--- website/cacheRedis.py
+++ website/cacheRedis.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 class cacheRedis:
     def __init__(self):
@@ -10,8 +11,29 @@ class cacheRedis:
             portNum = lines[1].strip()
             pwd = lines[2].strip()
 
-        self.__r = redis.Redis(host=host, port=portNum, username="default", password=pwd)
-        print(self.__r.ping())
+        self.__redisClient = redis.Redis(host=host, port=portNum, username="default", password=pwd)
 
-        #self.__r.set("TestKey1", "TestKeyValue1")
+        if self.__redisClient.ping():
+            print("Successfully connected to Redis cloud database!")
+        else:
+            print("Could not connect!")
+
+
+    def insertJSONData(self, TTL, key, jsonValue):
+        formattedData = json.dumps(jsonValue)
+        self.__redisClient.setex(key, TTL, formattedData)
+        print(f"Data inserted: {self.__redisClient.get(key)}, TTL (in seconds): {TTL}")
+
+    def checkIfJSONDocExistsAtKey(self, key):
+        if self.__redisClient.get(key) is not None:
+            print(f"JSON Object: {json.loads(self.__redisClient.get(key))}")
+            return True 
+        else:
+            print(f"No JSON object found at key: '{key}'")
+        return False
+
+
 testCacheRedis = cacheRedis()
+testCacheRedis.insertJSONData(20, 'user:1000', {"AthleteID": 5032, "AthleteName": "Alejandro", "SportType": "Run"})
+testCacheRedis.checkIfJSONDocExistsAtKey('user:1000')
+testCacheRedis.checkIfJSONDocExistsAtKey('user:1001')
