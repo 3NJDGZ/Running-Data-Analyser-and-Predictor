2f73962dbf21fc1ddddf5db8cad5e26ca551c3f6
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-11-17 15:14:02 +0000

    worked on datavisuals.py, activity data working, need to integrate pie chart creation

--- website/cachingService/cachingSystem.py
+++ website/cachingService/cachingSystem.py
@@ -8,24 +8,27 @@ class CachingSystem:
         self.__cacheClient = cacheClient
         self.__dbClient = dbClient
 
+    def getHRActivityData(self, client: Client):
+        pass
+
     def getRunningActivitiesID(self, client: Client):
         activities = client.get_activities(None, None, 15) 
-        activity_ids = []  # get the unique ids of each activity so we can get the 'detailed' activities object via the 'get_activity()' function
+        activityIDs = []  # get the unique ids of each activity so we can get the 'detailed' activities object via the 'get_activity()' function
         for activity in activities:
             rst = activity.sport_type # relaxed sport type
-            if rst.root == "Run" and len(activity_ids) <= 4:
+            if rst.root == "Run" and len(activityIDs) <= 4:
                 print(activity.id)
-                activity_ids.append(activity.id)
-        return activity_ids
+                activityIDs.append(activity.id)
+        return activityIDs
 
     
     def getActivityData(self, client: Client):
-        strava_athlete = client.get_athlete()
-        activity_data = []  # will be used to hold data
-        activity_ids = self.getRunningActivitiesID(client)
+        stravaAthlete = client.get_athlete()
+        activityDataToDisplay = []  # will be used to hold data
+        activityIDs = self.getRunningActivitiesID(client)
         # check if data entry already exists in mongoDB, if not then insert into database
-        for activityID in activity_ids:
-            if self.__cacheClient.getDataFromKey(self.__cacheClient.createKey(activityID)) is None and self.__dbClient.retrieveRunningData(activityID, strava_athlete.id) is None:
+        for activityID in activityIDs:
+            if self.__cacheClient.getDataFromKey(self.__cacheClient.createKey(activityID)) is None and self.__dbClient.retrieveRunningData(activityID, stravaAthlete.id) is None:
                 print("Data not found in both redis DB and MongoDB!")
                 activityName = client.get_activity(activityID).name
                 averageHeartRate = client.get_activity(activityID).average_heartrate
@@ -36,15 +39,15 @@ class CachingSystem:
                 elevationGain = elevationHigh - elevationLow
                 elevationGain = round(elevationGain, 1)
 
-                activity_streams = client.get_activity_streams(
+                activityStreams = client.get_activity_streams(
                     activityID, types=["heartrate"], resolution="low"
                 )
 
                 # getting HR stream data
-                if "heartrate" in activity_streams.keys():
-                    hrStream = activity_streams["heartrate"].data
+                if "heartrate" in activityStreams.keys():
+                    HRStream = activityStreams["heartrate"].data
                 else:
-                    hrStream = [0]
+                    HRStream = [0]
 
                 # predict intensity of the run
                 predictedIntensity = kmeans_predictor.predict(
@@ -58,12 +61,14 @@ class CachingSystem:
                         "elapsed_time": elapsedTime,
                         "elevation_gain": elevationGain,
                         "predicted_intensity": predictedIntensity,
-                        "activity_name": activityName
+                        "activity_name": activityName,
+                        "HRStream": HRStream
                     }
 
-                activity_data.append(dataToBeAdded)
+                activityDataToDisplay.append(dataToBeAdded)
 
-                self.__dbClient.insertRunningData(strava_athlete.id, client.get_activity(activityID).id, distance, elapsedTime, elevationHigh, elevationLow, predictedIntensity, averageHeartRate, activityName, strava_athlete.firstname, hrStream)
+                # insert data into mongoDB
+                self.__dbClient.insertRunningData(stravaAthlete.id, client.get_activity(activityID).id, distance, elapsedTime, elevationHigh, elevationLow, predictedIntensity, averageHeartRate, activityName, stravaAthlete.firstname, HRStream)
                 
                 # insert data into Redis
                 self.__cacheClient.insertJSONData(600, self.__cacheClient.createKey(activityID), dataToBeAdded)
@@ -73,7 +78,7 @@ class CachingSystem:
 
                 if dataToBeAdded is not None:
                     # data formatting  
-                    activity_data.append(
+                    activityDataToDisplay.append(
                         {
                             "average_heart_rate": dataToBeAdded["average_heart_rate"],
                             "distance": dataToBeAdded["distance"],
@@ -88,18 +93,19 @@ class CachingSystem:
                 else:
                     print("Data does not exist on redis DB! ")
                     print("Getting data from mongoDB!")
-                    dataToBeAdded = self.__dbClient.retrieveRunningData(activityID, strava_athlete.id)
+                    dataToBeAdded = self.__dbClient.retrieveRunningData(activityID, stravaAthlete.id)
 
-                    test = {
+                    jsonRedisData = {
                         "average_heart_rate": dataToBeAdded["AVGHR"],
                         "distance": dataToBeAdded["Distance"],
                         "elapsed_time": dataToBeAdded["ElapsedTime"],
                         "elevation_gain": round((dataToBeAdded["ElevationH"] - dataToBeAdded["ElevationL"]), 1),
                         "predicted_intensity": dataToBeAdded["PredictedIntensity"],
-                        "activity_name": dataToBeAdded["ActivityName"]
+                        "activity_name": dataToBeAdded["ActivityName"],
+                        "HRStream": dataToBeAdded["HRStream"]
                         }
 
-                    activity_data.append(
+                    activityDataToDisplay.append(
                         {
                             "average_heart_rate": dataToBeAdded["AVGHR"],
                             "distance": dataToBeAdded["Distance"],
@@ -109,11 +115,10 @@ class CachingSystem:
                             "activity_name": dataToBeAdded["ActivityName"]
                         }
                     )
-                    print(test)
 
                     # insert into redis database
-                    self.__cacheClient.insertJSONData(600, self.__cacheClient.createKey(activityID), test)
+                    self.__cacheClient.insertJSONData(600, self.__cacheClient.createKey(activityID), jsonRedisData)
                     print("Inserting data into redis DB!")
 
-        return activity_data
+        return activityDataToDisplay
         
--- website/dataVisuals.py
+++ website/dataVisuals.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from flask import render_template
 from website.baseView import baseView
 from stravalib import Client
-from kMeansClustering import kmeans_predictor
+from website.cachingService.cachingSystem import CachingSystem
 import json
 import matplotlib
 
@@ -18,9 +18,82 @@ import matplotlib.pyplot as plt
 
 # wrap auth routes in a class for OOP
 class dataVisualsRoutes(baseView):
-    def __init__(self, flaskApp, cachingSystem):
+    def __init__(self, flaskApp, cachingSystem: CachingSystem):
         super().__init__(flaskApp)
         self.__cachingSystem = cachingSystem
+
+    def createHRPieChart(self, formmattedHRData):
+        # create the figure
+        heart_rate_data = formmattedHRData 
+        zone1 = len([hr for hr in heart_rate_data if 0 <= hr <= 120])
+        zone2 = len([hr for hr in heart_rate_data if 121 <= hr <= 140])
+        zone3 = len([hr for hr in heart_rate_data if 141 <= hr <= 160])
+        zone4 = len([hr for hr in heart_rate_data if 161 <= hr <= 180])
+        zone5 = len([hr for hr in heart_rate_data if 181 <= hr <= 210])
+
+        # Create lists for values and labels
+        zones_value = [zone1, zone2, zone3, zone4, zone5]
+        zones_title = [
+            "Zone 1 (0-120 bpm)",
+            "Zone 2 (121-140 bpm)",
+            "Zone 3 (141-160 bpm)",
+            "Zone 4 (161-180 bpm)",
+            "Zone 5 (181-210 bpm)",
+        ]
+
+        # Create the pie chart
+        plt.figure(figsize=(8, 8))
+
+        # Define custom colors for better distinction
+        colors = [
+            "#ff9999",
+            "#66b3ff",
+            "#99ff99",
+            "#ffcc99",
+            "#c2c2f0",
+        ]  # Customize colors here
+
+        # Explode the slices for better visibility
+        explode = (
+            0.1,
+            0,
+            0,
+            0,
+            0,
+        )  # Only "explode" the first slice (Zone 1)
+
+        wedges, texts, autotexts = plt.pie(
+            zones_value,
+            labels=None,  # Set labels to None for the pie chart
+            autopct="%1.1f%%",
+            startangle=140,
+            colors=colors,
+            explode=explode,
+        )
+
+        # Adding the legend outside the pie chart
+        plt.legend(
+            wedges,
+            zones_title,
+            title="Heart Rate Zones",
+            loc="center left",
+            bbox_to_anchor=(1, 0, 0.5, 1),
+        )
+
+        # Improve aesthetics
+        plt.title("Heart Rate Zones Distribution", fontsize=16)
+        plt.axis(
+            "equal"
+        )  # Equal aspect ratio ensures the pie chart is circular.
+
+        # Save and show the plot
+        plt.savefig(
+            "website/static/statisticalGraphsAndCharts/heart_rate_zones.png",
+            bbox_inches="tight",
+        )
+        plt.show()
+
+
 
     def _setupRoutes(self):
         # setup routes
@@ -32,108 +105,39 @@ class dataVisualsRoutes(baseView):
             # Create client object again to retain session
             client = Client(access_token=access_token_file["access_token"])
             stravaAthlete = client.get_athlete()
-            stravaAthleteID = stravaAthlete.id
-            retrievedData = self.__mongoDB.retrieveAthleteActivities(stravaAthleteID)
-            activity_data = []
-            hrData = []
-            formmattedHRData = []
-            
-            # format data and append to activity data so we can show stats for each activity
-            for doc in retrievedData:
-                activity_data.append(
-                    {
-                        "average_heart_rate": doc["AVGHR"],
-                        "distance": doc["Distance"],
-                        "elapsed_time": doc["ElapsedTime"],
-                        "elevation_gain": round((doc["ElevationH"] - doc["ElevationL"]), 1),
-                        "predicted_intensity": doc["PredictedIntensity"],
-                        "activity_name": doc["ActivityName"]
-
-                    }
-                )
-
-            # get stream data for each activity
-            for doc in retrievedData:
-                activityID = doc["ActivityID"]
-                streamData = client.get_activity_streams(activityID, types=["heartrate"], resolution="low")
-                hrData.append(streamData["heartrate"].data)
-
-            # format all stream data into a singular array
-            for stream in hrData:
-                formmattedHRData = formmattedHRData + stream
-            formmattedHRData = sorted(formmattedHRData)
-
-            # create the figure
-            heart_rate_data = formmattedHRData 
-            zone1 = len([hr for hr in heart_rate_data if 0 <= hr <= 120])
-            zone2 = len([hr for hr in heart_rate_data if 121 <= hr <= 140])
-            zone3 = len([hr for hr in heart_rate_data if 141 <= hr <= 160])
-            zone4 = len([hr for hr in heart_rate_data if 161 <= hr <= 180])
-            zone5 = len([hr for hr in heart_rate_data if 181 <= hr <= 210])
-
-            # Create lists for values and labels
-            zones_value = [zone1, zone2, zone3, zone4, zone5]
-            zones_title = [
-                "Zone 1 (0-120 bpm)",
-                "Zone 2 (121-140 bpm)",
-                "Zone 3 (141-160 bpm)",
-                "Zone 4 (161-180 bpm)",
-                "Zone 5 (181-210 bpm)",
-            ]
-
-            # Create the pie chart
-            plt.figure(figsize=(8, 8))
-
-            # Define custom colors for better distinction
-            colors = [
-                "#ff9999",
-                "#66b3ff",
-                "#99ff99",
-                "#ffcc99",
-                "#c2c2f0",
-            ]  # Customize colors here
-
-            # Explode the slices for better visibility
-            explode = (
-                0.1,
-                0,
-                0,
-                0,
-                0,
-            )  # Only "explode" the first slice (Zone 1)
-
-            wedges, texts, autotexts = plt.pie(
-                zones_value,
-                labels=None,  # Set labels to None for the pie chart
-                autopct="%1.1f%%",
-                startangle=140,
-                colors=colors,
-                explode=explode,
-            )
-
-            # Adding the legend outside the pie chart
-            plt.legend(
-                wedges,
-                zones_title,
-                title="Heart Rate Zones",
-                loc="center left",
-                bbox_to_anchor=(1, 0, 0.5, 1),
-            )
-
-            # Improve aesthetics
-            plt.title("Heart Rate Zones Distribution", fontsize=16)
-            plt.axis(
-                "equal"
-            )  # Equal aspect ratio ensures the pie chart is circular.
-
-            # Save and show the plot
-            plt.savefig(
-                "website/static/statisticalGraphsAndCharts/heart_rate_zones.png",
-                bbox_inches="tight",
-            )
-            plt.show()
-
+            activityData = self.__cachingSystem.getActivityData(client)
+            for activity in activityData:
+                print(activity)
+            # retrievedData = self.__mongoDB.retrieveAthleteActivities(stravaAthleteID)
+            # activity_data = []
+            # hrData = []
+            # formmattedHRData = []
+            #
+            # # format data and append to activity data so we can show stats for each activity
+            # for doc in retrievedData:
+            #     activity_data.append(
+            #         {
+            #             "average_heart_rate": doc["AVGHR"],
+            #             "distance": doc["Distance"],
+            #             "elapsed_time": doc["ElapsedTime"],
+            #             "elevation_gain": round((doc["ElevationH"] - doc["ElevationL"]), 1),
+            #             "predicted_intensity": doc["PredictedIntensity"],
+            #             "activity_name": doc["ActivityName"]
+            #
+            #         }
+            #     )
+            #
+            # # get stream data for each activity
+            # for doc in retrievedData:
+            #     activityID = doc["ActivityID"]
+            #     streamData = client.get_activity_streams(activityID, types=["heartrate"], resolution="low")
+            #     hrData.append(streamData["heartrate"].data)
+            #
+            # # format all stream data into a singular array
+            # for stream in hrData:
+            #     formmattedHRData = formmattedHRData + stream
+            # formmattedHRData = sorted(formmattedHRData)
             return render_template(
                 "data_visuals.html",
-                activity_data=activity_data,
+                activity_data=activityData,
             )
