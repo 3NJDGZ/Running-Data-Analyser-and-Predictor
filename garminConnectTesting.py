e04afb06576ff916865a498489a71e2ac5743515
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2024-11-22 21:40:37 +0000

    figuring out api connection login

--- garminConnectTesting.py
+++ garminConnectTesting.py
@@ -1,7 +1,11 @@
 import garminconnect as garmin 
+import os 
 
-tokenstore = "~/.garminconnect"
-garminClient = garmin.Garmin()
+tokenstore = os.getenv("GARMINTOKENS") or "~/.garminconnect"
+email = input("Enter Email: ")
+pwd = input("Enter password: ")
+garminClient = garmin.Garmin(email=email, password=pwd)
 
-garminClient.login(tokenstore)
+garminClient.login()
+garminClient.garth.dump(tokenstore)
 print(f"Full name: {garminClient.get_full_name()}")
